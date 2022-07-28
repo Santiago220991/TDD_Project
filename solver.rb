70682e3921bae8d211227475eee13f99102346ae
Santiago <alexander.cardenas10@outlook.com>
2022-07-28 13:00:49 -0500

    Add solver class with factorial method

---
+++ solver.rb
@@ -0,0 +1,10 @@
+class Solver
+    def factorial(num)
+        if(num>=0)
+            return 1 if num==0
+        num*factorial(num-1)
+        else
+        raise "Number should be positive"
+        end
+    end
+end
