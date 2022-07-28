bbb5d5e28d8c80e1dce6c0ee337931732f096320
Ronald <rjskinnerc@unal.edu.co>
2022-07-28 13:17:52 -0500

    Add fizzbuzz method

--- solver.rb
+++ solver.rb
@@ -11,4 +11,16 @@ class Solver
     def reverse(string)
         string.reverse
     end
+
+    def fizzbuzz(n)
+        if n%3 == 0 && n%5 == 0 
+            return 'fizzbuzz'
+        elsif n%3 == 0
+            return 'fizz'
+        elsif n%5 == 0
+            return 'buzz'
+        else 
+            n.to_s
+        end
+    end
 end
